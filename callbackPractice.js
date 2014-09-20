001602f06a26fcfb7ad2daf6b7ab47464d58dbc2
spencerwencer <spencerwise@spencers-mbp-3.home>
2014-09-20 12:12:15 -0600

    First done

--- callbackPractice.js
+++ callbackPractice.js
@@ -25,7 +25,9 @@ and what you should write is the favNum function that makes the code above work,
 */
 
 
-  //Code Here for first
+var first = function(list, cb) {
+  cb(list[0]);
+}
   
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
